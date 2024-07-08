8859ddae3938c30dd0ac291ccaaef5314c462961
koniecdev <koniecdev@gmail.com>
2024-07-09 01:02:44 +0200

    Persistence set up

---
+++ KittySaver/Persistence/Factories/ApplicationDbContextFactory.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace KittySaver.Persistence.Factories;
+
+internal sealed class ApplicationDbContextFactoryFactory : DesignTimeDbContextFactoryBase<ApplicationDbContext>
+{
+    protected override ApplicationDbContext CreateNewInstance(DbContextOptions<ApplicationDbContext> options)
+    {
+        var db = new ApplicationDbContext(options);
+        return db;
+    }
+}
