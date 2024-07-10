d3732776a87da3e795836681485067219f087e8d
koniecdev <koniecdev@gmail.com>
2024-07-11 00:01:08 +0200

    Exceptions handler registering delegated to extension method

---
+++ KittySaver.Api/Extensions/ExceptionHandlersExtensions.cs
@@ -0,0 +1,15 @@
+﻿using KittySaver.Api.Middlewares.ExceptionHandlers;
+
+namespace KittySaver.Api.Extensions;
+
+internal static class ExceptionHandlersExtensions
+{
+    internal static IServiceCollection AddEveryExceptionHandler(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddExceptionHandler<NotFoundExceptionHandler>();
+        serviceCollection.AddExceptionHandler<BadRequestExceptionHandler>();
+        serviceCollection.AddExceptionHandler<GlobalExceptionHandler>();
+        return serviceCollection;
+    }
+}
+
