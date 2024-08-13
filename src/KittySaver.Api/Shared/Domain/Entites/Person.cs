277df6bfdc977c7267d345e9204ce20a714adb05
koniecdev <koniecdev@gmail.com>
2024-08-14 00:19:33 +0200

    Person entity; removed useless custom exceptions and validation

--- src/KittySaver.Api/Shared/Domain/Entites/Person.cs
+++ src/KittySaver.Api/Shared/Domain/Entites/Person.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
 using KittySaver.Api.Shared.Exceptions;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -6,18 +7,45 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace KittySaver.Api.Shared.Domain.Entites;
 
-public class Person : IdentityUser<Guid>
+public sealed class Person : IdentityUser<Guid>
 {
-    private static readonly string EmailPattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
-    private static readonly string PhonePattern = @"^\+?[1-9]\d{1,14}$";
+    private static readonly string EmailPattern = @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$";
     private string? _userName;
     private string? _email;
     private string? _phoneNumber;
+    private string _firstName = null!;
+    private string _lastName = null!;
 
-    public override required string? UserName
+    public required string FirstName
+    {
+        get => _firstName;
+        init
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(value, nameof(FirstName));
+            string firstChar = value[0].ToString().ToUpper();
+            string rest = value[1..].ToLower();
+            _firstName = $"{firstChar}{rest}";
+        } 
+    }
+
+    public required string LastName
+    {
+        get => _lastName;
+        init
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(value, nameof(LastName));
+            string firstChar = value[0].ToString().ToUpper();
+            string rest = value[1..].ToLower();
+            _lastName = $"{firstChar}{rest}";
+        } 
+    }
+
+    public string FullName => $"{FirstName} {LastName}";
+    public override string? UserName
     {
         get => _userName;
-        set {
+        set 
+        {
             if (value != Email)
             {
                 _userName = Email;
@@ -31,10 +59,7 @@ public class Person : IdentityUser<Guid>
         get => _email;
         set
         {
-            if (string.IsNullOrWhiteSpace(value))
-            {
-                throw new Exceptions.Email.EmptyException();
-            }
+            ArgumentException.ThrowIfNullOrWhiteSpace(value, nameof(Email));
             if (!Regex.IsMatch(value, EmailPattern))
             {
                 throw new Exceptions.Email.InvalidFormatException();
@@ -49,14 +74,7 @@ public class Person : IdentityUser<Guid>
         get => _phoneNumber;
         set
         {
-            if (string.IsNullOrWhiteSpace(value))
-            {
-                throw new Exceptions.PhoneNumber.EmptyException();
-            }
-            if (!Regex.IsMatch(value, EmailPattern))
-            {
-                throw new Exceptions.PhoneNumber.InvalidFormatException();
-            }
+            ArgumentException.ThrowIfNullOrWhiteSpace(value, nameof(PhoneNumber));
             _phoneNumber = value;
         }
     }
@@ -68,20 +86,10 @@ public class Person : IdentityUser<Guid>
         
         public static class Email
         {
-            public sealed class EmptyException() 
-                : BadRequestException("Person.Email.Empty", "Email is empty");
             public sealed class InvalidFormatException() 
                 : BadRequestException("Person.Email.InvalidFormat", "Email format is invalid");
             public sealed class NotUniqueException() 
                 : BadRequestException("Person.Email.NotUnique", "Email is not unique");
-        }
-
-        public static class PhoneNumber
-        {
-            public sealed class EmptyException() 
-                : BadRequestException("Person.PhoneNumber.Empty", "Phone number is empty");
-            public sealed class InvalidFormatException() 
-                : BadRequestException("Person.PhoneNumber.InvalidFormat", "Phone number is invalid");
         }
     }
 }
@@ -90,6 +98,14 @@ internal class PersonConfiguration : IEntityTypeConfiguration<Person>
 {
     public void Configure(EntityTypeBuilder<Person> builder)
     {
+        builder
+            .Property(m=>m.FirstName)
+            .HasMaxLength(50)
+            .IsRequired();
+        builder
+            .Property(m=>m.LastName)
+            .HasMaxLength(50)
+            .IsRequired();
         builder
             .Property(m=>m.Email)
             .HasMaxLength(254)
