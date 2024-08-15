4cba4b13062eea9196b3806040211e83f43a2056
koniecdev <koniecdev@gmail.com>
2024-08-15 17:18:15 +0200

    Roles seed created

---
+++ src/KittySaver.Api/Migrations/20240815151723_CreateRoles.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace KittySaver.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { new Guid("86c81ae7-b11c-4343-9577-872a74637200"), null, "Shelter", "Shelter" },
+                    { new Guid("b1a6bd96-90d6-4949-b618-ce662f41f87a"), null, "Administrator", "ADMINISTRATOR" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("86c81ae7-b11c-4343-9577-872a74637200"));
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("b1a6bd96-90d6-4949-b618-ce662f41f87a"));
+        }
+    }
+}
--- src/KittySaver.Api/Shared/Persistence/SeedData.cs
+++ src/KittySaver.Api/Shared/Persistence/SeedData.cs
@@ -1,39 +1,39 @@
-﻿// using KittySaver.Api.Shared.Domain.Entites;
-// using Microsoft.AspNetCore.Identity;
-// using Microsoft.EntityFrameworkCore;
-// using Microsoft.EntityFrameworkCore.Metadata.Builders;
-//
-// namespace KittySaver.Api.Shared.Persistence;
-//
-// public static class FixedIdsHelper
-// {
-//     public static Guid AdminRoleId { get; } = Guid.Parse("b1a6bd96-90d6-4949-b618-ce662f41f87a");
-//     public static Guid ShelterRoleId { get; } = Guid.Parse("86c81ae7-b11c-4343-9577-872a74637200");
-//     public static Guid AdminId { get; } = Guid.Parse("c4ff57f4-2d8c-4c66-b62d-333ad2e3c424");
-//     public static Guid AdminUserIdentityId { get; } = Guid.Parse("a4018ea1-525a-48eb-a701-a96c1a261e72");
-//     public static Guid AdminStamp { get; } = Guid.Parse("34d18c72-0a52-4bbe-9b00-754a02e2293e");
-// }
-// public class RoleConfiguration : IEntityTypeConfiguration<IdentityRole<Guid>>
-// {
-//     
-//     public void Configure(EntityTypeBuilder<IdentityRole<Guid>> builder)
-//     {
-//         builder.HasData(
-//             new IdentityRole<Guid>
-//             {
-//                 Id = FixedIdsHelper.AdminRoleId,
-//                 Name = "Administrator",
-//                 NormalizedName = "ADMINISTRATOR"
-//             },
-//             new IdentityRole<Guid>
-//             {
-//                 Id = FixedIdsHelper.ShelterRoleId,
-//                 Name = "Shelter",
-//                 NormalizedName = "Shelter"
-//             }
-//         );
-//     }
-// }
+﻿using KittySaver.Api.Shared.Domain.Entites;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace KittySaver.Api.Shared.Persistence;
+
+public static class FixedIdsHelper
+{
+    public static Guid AdminRoleId { get; } = Guid.Parse("b1a6bd96-90d6-4949-b618-ce662f41f87a");
+    public static Guid ShelterRoleId { get; } = Guid.Parse("86c81ae7-b11c-4343-9577-872a74637200");
+    public static Guid AdminId { get; } = Guid.Parse("c4ff57f4-2d8c-4c66-b62d-333ad2e3c424");
+    public static Guid AdminUserIdentityId { get; } = Guid.Parse("a4018ea1-525a-48eb-a701-a96c1a261e72");
+    public static Guid AdminStamp { get; } = Guid.Parse("34d18c72-0a52-4bbe-9b00-754a02e2293e");
+}
+public class RoleConfiguration : IEntityTypeConfiguration<IdentityRole<Guid>>
+{
+    
+    public void Configure(EntityTypeBuilder<IdentityRole<Guid>> builder)
+    {
+        builder.HasData(
+            new IdentityRole<Guid>
+            {
+                Id = FixedIdsHelper.AdminRoleId,
+                Name = "Administrator",
+                NormalizedName = "ADMINISTRATOR"
+            },
+            new IdentityRole<Guid>
+            {
+                Id = FixedIdsHelper.ShelterRoleId,
+                Name = "Shelter",
+                NormalizedName = "Shelter"
+            }
+        );
+    }
+}
 //
 // public class AdminConfiguration(IConfiguration configuration) : IEntityTypeConfiguration<Person>
 // {
