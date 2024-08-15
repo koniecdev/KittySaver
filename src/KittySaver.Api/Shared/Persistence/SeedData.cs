cdca06e391876602614c901913b87ac5d8cb6f1c
koniecdev <koniecdev@gmail.com>
2024-08-15 16:53:14 +0200

    Migration of Identity and Person tables

--- src/KittySaver.Api/Shared/Persistence/SeedData.cs
+++ src/KittySaver.Api/Shared/Persistence/SeedData.cs
@@ -1,81 +1,85 @@
-﻿using KittySaver.Api.Shared.Domain.Entites;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
-
-namespace KittySaver.Api.Shared.Persistence;
-
-public class RoleConfiguration : IEntityTypeConfiguration<IdentityRole<Guid>>
-{
-    private readonly Guid _adminId = Guid.Parse("b1a6bd96-90d6-4949-b618-ce662f41f87a");
-    private readonly Guid _shelterId = Guid.Parse("86c81ae7-b11c-4343-9577-872a74637200");
-    
-    public void Configure(EntityTypeBuilder<IdentityRole<Guid>> builder)
-    {
-        builder.HasData(
-            new IdentityRole<Guid>
-            {
-                Id = _adminId,
-                Name = "Administrator",
-                NormalizedName = "ADMINISTRATOR"
-            },
-            new IdentityRole<Guid>
-            {
-                Id = _shelterId,
-                Name = "Shelter",
-                NormalizedName = "Shelter"
-            }
-        );
-    }
-}
-
-public class AdminConfiguration(IConfiguration configuration) : IEntityTypeConfiguration<Person>
-{
-    private readonly Guid _adminId = Guid.Parse("c4ff57f4-2d8c-4c66-b62d-333ad2e3c424");
-
-    public void Configure(EntityTypeBuilder<Person> builder)
-    {
-        Person admin = new()
-        {
-            Id = _adminId,
-            UserName = "defaultadmin@koniec.dev",
-            NormalizedUserName = "DEFAULTADMIN@KONIEC.DEV",
-            FirstName = "Default",
-            LastName = "Admin",
-            Email = "defaultadmin@koniec.dev",
-            NormalizedEmail = "DEFAULTADMIN@KONIEC.DEV",
-            PhoneNumber = "XXXXXXXXX",
-            EmailConfirmed = true,
-            PhoneNumberConfirmed = true,
-            SecurityStamp = new Guid().ToString("D"),
-            UserId = Guid.Parse("c35f84aa-9c22-4567-a529-174789e02a4c")
-        };
-
-        admin.PasswordHash = PassGenerate(admin);
-
-        builder.HasData(admin);
-    }
-
-    private string PassGenerate(Person user)
-    {
-        PasswordHasher<Person> passHash = new();
-        return passHash.HashPassword(user, configuration["Data:DefaultAdminPassword"]!);
-    }
-}
-
-public class UsersWithRolesConfiguration : IEntityTypeConfiguration<IdentityUserRole<Guid>>
-{
-    private readonly Guid _adminUserId = Guid.Parse("c4ff57f4-2d8c-4c66-b62d-333ad2e3c424");
-    private readonly Guid _adminRoleId = Guid.Parse("b1a6bd96-90d6-4949-b618-ce662f41f87a");
-
-    public void Configure(EntityTypeBuilder<IdentityUserRole<Guid>> builder)
-    {
-        IdentityUserRole<Guid> iur = new IdentityUserRole<Guid>
-        {
-            RoleId = _adminRoleId,
-            UserId = _adminUserId
-        };
-
-        builder.HasData(iur);
-    }
-}
+﻿// using KittySaver.Api.Shared.Domain.Entites;
+// using Microsoft.AspNetCore.Identity;
+// using Microsoft.EntityFrameworkCore;
+// using Microsoft.EntityFrameworkCore.Metadata.Builders;
+//
+// namespace KittySaver.Api.Shared.Persistence;
+//
+// public static class FixedIdsHelper
+// {
+//     public static Guid AdminRoleId { get; } = Guid.Parse("b1a6bd96-90d6-4949-b618-ce662f41f87a");
+//     public static Guid ShelterRoleId { get; } = Guid.Parse("86c81ae7-b11c-4343-9577-872a74637200");
+//     public static Guid AdminId { get; } = Guid.Parse("c4ff57f4-2d8c-4c66-b62d-333ad2e3c424");
+//     public static Guid AdminUserIdentityId { get; } = Guid.Parse("a4018ea1-525a-48eb-a701-a96c1a261e72");
+//     public static Guid AdminStamp { get; } = Guid.Parse("34d18c72-0a52-4bbe-9b00-754a02e2293e");
+// }
+// public class RoleConfiguration : IEntityTypeConfiguration<IdentityRole<Guid>>
+// {
+//     
+//     public void Configure(EntityTypeBuilder<IdentityRole<Guid>> builder)
+//     {
+//         builder.HasData(
+//             new IdentityRole<Guid>
+//             {
+//                 Id = FixedIdsHelper.AdminRoleId,
+//                 Name = "Administrator",
+//                 NormalizedName = "ADMINISTRATOR"
+//             },
+//             new IdentityRole<Guid>
+//             {
+//                 Id = FixedIdsHelper.ShelterRoleId,
+//                 Name = "Shelter",
+//                 NormalizedName = "Shelter"
+//             }
+//         );
+//     }
+// }
+//
+// public class AdminConfiguration(IConfiguration configuration) : IEntityTypeConfiguration<Person>
+// {
+//     public void Configure(EntityTypeBuilder<Person> builder)
+//     {
+//         Person admin = new()
+//         {
+//             Id = FixedIdsHelper.AdminId,
+//             UserName = "defaultadmin@koniec.dev",
+//             NormalizedUserName = "DEFAULTADMIN@KONIEC.DEV",
+//             FirstName = "Default",
+//             LastName = "Admin",
+//             Email = "defaultadmin@koniec.dev",
+//             NormalizedEmail = "DEFAULTADMIN@KONIEC.DEV",
+//             PhoneNumber = "XXXXXXXXX",
+//             EmailConfirmed = true,
+//             PhoneNumberConfirmed = true,
+//             SecurityStamp = FixedIdsHelper.AdminStamp.ToString(),
+//             UserIdentityId = FixedIdsHelper.AdminUserIdentityId
+//         };
+//
+//         admin.PasswordHash = PassGenerate(admin);
+//
+//         builder.HasData(admin);
+//     }
+//
+//     private string PassGenerate(Person user)
+//     {
+//         PasswordHasher<Person> passHash = new();
+//         return passHash.HashPassword(user, configuration["Data:DefaultAdminPassword"]!);
+//     }
+// }
+//
+// public class UsersWithRolesConfiguration : IEntityTypeConfiguration<IdentityUserRole<Guid>>
+// {
+//     private readonly Guid _adminId = FixedIdsHelper.AdminId;
+//     private readonly Guid _adminRoleId = FixedIdsHelper.AdminRoleId;
+//
+//     public void Configure(EntityTypeBuilder<IdentityUserRole<Guid>> builder)
+//     {
+//         IdentityUserRole<Guid> iur = new IdentityUserRole<Guid>
+//         {
+//             RoleId = _adminRoleId,
+//             UserId = _adminId
+//         };
+//
+//         builder.HasData(iur);
+//     }
+// }
