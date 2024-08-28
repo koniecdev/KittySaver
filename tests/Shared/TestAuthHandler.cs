213a541ac29997c08470684ca4263bc0a7fc8c77
koniecdev <koniecdev@gmail.com>
2024-08-28 12:05:13 +0200

    Tests shared project created for common tests logic

---
+++ tests/Shared/TestAuthHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Claims;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Shared;
+
+public class TestAuthHandler(
+    IOptionsMonitor<AuthenticationSchemeOptions> options,
+    ILoggerFactory logger,
+    UrlEncoder encoder)
+    : AuthenticationHandler<AuthenticationSchemeOptions>(options, logger, encoder)
+{
+    protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+        // Here we set up the claims that your application needs
+        Claim[] claims = [
+            new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()),
+            new Claim(ClaimTypes.Role, "SuperAdmin"),
+            new Claim(ClaimTypes.Email, "defaultadmin@koniec.dev")
+        ];
+
+        ClaimsIdentity identity = new ClaimsIdentity(claims, "TestAuthType");
+        ClaimsPrincipal principal = new ClaimsPrincipal(identity);
+        AuthenticationTicket ticket = new AuthenticationTicket(principal, "TestScheme");
+
+        return Task.FromResult(AuthenticateResult.Success(ticket));
+    }
+}
