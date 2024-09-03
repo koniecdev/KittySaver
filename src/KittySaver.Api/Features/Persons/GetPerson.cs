121ed400978c6260a7c774d81960440c3012ba83
koniecdev <koniecdev@gmail.com>
2024-09-03 18:13:36 +0200

    Get single person feature added

---
+++ src/KittySaver.Api/Features/Persons/GetPerson.cs
@@ -0,0 +1,39 @@
+﻿using KittySaver.Api.Features.Persons.SharedContracts;
+using KittySaver.Api.Shared.Infrastructure.ApiComponents;
+using KittySaver.Api.Shared.Infrastructure.Endpoints;
+using KittySaver.Api.Shared.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace KittySaver.Api.Features.Persons;
+
+public class GetPerson : IEndpoint
+{
+    public sealed class GetPersonsQuery : IQuery<ICollection<PersonResponse>>
+    {
+    }
+
+    internal sealed class GetPersonsQueryHandler(ApplicationDbContext db)
+        : IRequestHandler<GetPersonsQuery, ICollection<PersonResponse>>
+    {
+        public async Task<ICollection<PersonResponse>> Handle(GetPersonsQuery request, CancellationToken cancellationToken)
+        {
+            List<PersonResponse> persons = await db.Persons
+                .AsNoTracking()
+                .ProjectToDto()
+                .ToListAsync(cancellationToken);
+            return persons;
+        }
+    }
+
+    public void MapEndpoint(IEndpointRouteBuilder endpointRouteBuilder)
+    {
+        endpointRouteBuilder.MapGet("persons", async (ISender sender,
+            CancellationToken cancellationToken) =>
+        {
+            GetPersonsQuery query = new();
+            ICollection<PersonResponse> persons = await sender.Send(query, cancellationToken);
+            return Results.Ok(persons);
+        }).RequireAuthorization();
+    }
+}
