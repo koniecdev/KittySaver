2d2a6fc677fca1cd75e03e9282fc46b76db89d85
koniecdev <koniecdev@gmail.com>
2024-09-29 20:25:39 +0200

    update person feature got updated validation

--- src/KittySaver.Api/Features/Persons/UpdatePerson.cs
+++ src/KittySaver.Api/Features/Persons/UpdatePerson.cs
@@ -2,7 +2,6 @@
 using KittySaver.Api.Features.Persons.SharedContracts;
 using KittySaver.Api.Shared.Domain.Entites;
 using KittySaver.Api.Shared.Infrastructure.ApiComponents;
-using KittySaver.Api.Shared.Infrastructure.Endpoints;
 using KittySaver.Api.Shared.Persistence;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -29,30 +28,30 @@ public sealed class UpdatePerson : IEndpoint
         : AbstractValidator<UpdatePersonCommand>, IAsyncValidator
     {
         private readonly ApplicationDbContext _db;
-
         public UpdatePersonCommandValidator(ApplicationDbContext db)
         {
             _db = db; 
+            RuleFor(x => x.Id).NotEmpty();
             RuleFor(x => x.FirstName).NotEmpty();
             RuleFor(x => x.LastName).NotEmpty();
             RuleFor(x => x.PhoneNumber).NotEmpty();
-            RuleFor(x => x.Id)
-                .MustAsync(async (id, ct) => await IsUserExistingInDatabase(id, ct))
-                .WithMessage("User must exist in database to delete it");
+            RuleFor(x => x.PhoneNumber)
+                .MustAsync(async (command, email, ct) => await IsPhoneUniqueAsync(command, email, ct))
+                .WithMessage("'Phone Number' is already used by another user.");
             RuleFor(x => x.Email)
                 .NotEmpty()
                 .Matches(ValidationPatterns.EmailPattern);
             RuleFor(x => x.Email)
-                .MustAsync(async (command, email, ct) => !await IsEmailAlreadyUsedBySomeoneElseInDb(command, email, ct))
-                .WithMessage("Email is already used by different account in database");
+                .MustAsync(async (command, email, ct) => await IsEmailUniqueAsync(command, email, ct))
+                .WithMessage("'Email' is already used by another user.");
         }
-
-        private async Task<bool> IsEmailAlreadyUsedBySomeoneElseInDb(UpdatePersonCommand command, string email, CancellationToken ct)
-        {
-            return await _db.Persons.AnyAsync(x => x.Email == email && x.Id != command.Id, ct);
-        } 
-        private async Task<bool> IsUserExistingInDatabase(Guid id, CancellationToken ct) 
-            => await _db.Persons.AnyAsync(x=>x.Id == id, ct);
+        private async Task<bool> IsEmailUniqueAsync(UpdatePersonCommand command, string email, CancellationToken ct)
+            =>!await _db.Persons
+                .AnyAsync(x=>x.Email == email && x.Id != command.Id, ct);
+        
+        private async Task<bool> IsPhoneUniqueAsync(UpdatePersonCommand command, string phone, CancellationToken ct)
+            => !await _db.Persons
+                .AnyAsync(x=>x.PhoneNumber == phone && x.Id != command.Id, ct);
     }
     
     internal sealed class UpdatePersonCommandHandler(ApplicationDbContext db)
