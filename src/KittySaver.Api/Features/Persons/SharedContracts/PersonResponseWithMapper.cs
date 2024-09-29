031715c98663bb3864dc2daaaf8c0f3ca076da6e
koniecdev <koniecdev@gmail.com>
2024-09-29 20:26:28 +0200

    PersonResponse now contains firstname and lastname

--- src/KittySaver.Api/Features/Persons/SharedContracts/PersonResponseWithMapper.cs
+++ src/KittySaver.Api/Features/Persons/SharedContracts/PersonResponseWithMapper.cs
@@ -6,6 +6,8 @@ namespace KittySaver.Api.Features.Persons.SharedContracts;
 public sealed class PersonResponse
 {
     public required Guid Id { get; init; }
+    public required string FirstName { get; init; }
+    public required string LastName { get; init; }
     public required string FullName { get; init; }
     public required string Email { get; init; }
     public required string PhoneNumber { get; init; }
