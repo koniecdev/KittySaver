a9e0ba26b2bde7da3a061d8b09b60e74696d6baa
koniecdev <koniecdev@gmail.com>
2024-10-06 19:06:02 +0200

    Delete cat feature added

---
+++ src/KittySaver.Api/Features/Cats/DeleteCat.cs
@@ -0,0 +1,51 @@
+﻿using FluentValidation;
+using KittySaver.Api.Shared.Domain.Entites;
+using KittySaver.Api.Shared.Infrastructure.ApiComponents;
+using KittySaver.Api.Shared.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace KittySaver.Api.Features.Cats;
+
+public sealed class DeleteCat : IEndpoint
+{
+    public sealed record DeleteCatCommand(Guid Id) : ICommand;
+
+    public sealed class DeleteCatCommandValidator
+        : AbstractValidator<DeleteCatCommand>
+    {
+        public DeleteCatCommandValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty();
+        }
+    }
+
+    internal sealed class DeleteCatCommandHandler(ApplicationDbContext db)
+        : IRequestHandler<DeleteCatCommand>
+    {
+        public async Task Handle(DeleteCatCommand request, CancellationToken cancellationToken)
+        {
+            int numberOfDeletedCats = await db.Cats
+                .Where(x => 
+                    x.Id == request.Id)
+                .ExecuteDeleteAsync(cancellationToken);
+            if (numberOfDeletedCats == 0)
+            {
+                throw new Cat.CatNotFoundException(request.Id);
+            }
+        }
+    }
+
+    public void MapEndpoint(IEndpointRouteBuilder endpointRouteBuilder)
+    {
+        endpointRouteBuilder.MapDelete("cats/{id:guid}", async (
+            Guid id,
+            ISender sender,
+            CancellationToken cancellationToken) =>
+        {
+            DeleteCatCommand command = new(id);
+            await sender.Send(command, cancellationToken);
+            return Results.NoContent();
+        }).RequireAuthorization();
+    }
+}
