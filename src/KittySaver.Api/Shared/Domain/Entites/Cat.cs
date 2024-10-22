3cca9d6900cc541d0d812912849547193803942d
koniecdev <koniecdev@gmail.com>
2024-10-22 13:41:23 +0200

    Cat entity modifications

--- src/KittySaver.Api/Shared/Domain/Entites/Cat.cs
+++ src/KittySaver.Api/Shared/Domain/Entites/Cat.cs
@@ -1,5 +1,7 @@
-﻿using KittySaver.Api.Shared.Domain.Entites.Common;
+﻿using System.Diagnostics.CodeAnalysis;
+using KittySaver.Api.Shared.Domain.Entites.Common;
 using KittySaver.Api.Shared.Domain.Enums;
+using KittySaver.Api.Shared.Domain.Services;
 using KittySaver.Api.Shared.Domain.ValueObjects;
 using KittySaver.Api.Shared.Exceptions;
 using Microsoft.EntityFrameworkCore;
@@ -9,8 +11,87 @@ namespace KittySaver.Api.Shared.Domain.Entites;
 
 public sealed class Cat : AuditableEntity
 {
-    public required string Name { get; set; }
-    public string? AdditionalRequirements { get; set; }
+    private string _name = null!;
+    private string? _additionalRequirements;
+
+    [SetsRequiredMembers]
+    public Cat(
+        Guid personId,
+        string name,
+        MedicalHelpUrgency medicalHelpUrgency,
+        AgeCategory ageCategory,
+        Behavior behavior,
+        HealthStatus healthStatus,
+        bool isCastrated,
+        bool isInNeedOfSeeingVet,
+        string? additionalRequirements)
+    {
+        PersonId = personId;
+        Name = name;
+        MedicalHelpUrgency = medicalHelpUrgency;
+        AgeCategory = ageCategory;
+        Behavior = behavior;
+        HealthStatus = healthStatus;
+        IsCastrated = isCastrated;
+        IsInNeedOfSeeingVet = isInNeedOfSeeingVet;
+        AdditionalRequirements = additionalRequirements;
+    }
+
+    public static Cat Create(
+        ICatPriorityCalculator calculator,
+        Guid personId,
+        string name,
+        MedicalHelpUrgency medicalHelpUrgency,
+        AgeCategory ageCategory,
+        Behavior behavior,
+        HealthStatus healthStatus,
+        bool isCastrated = false,
+        bool isInNeedOfSeeingVet = false,
+        string? additionalRequirements = null)
+    {
+        Cat cat = new(
+            personId: personId,
+            name: name,
+            medicalHelpUrgency: medicalHelpUrgency,
+            ageCategory: ageCategory,
+            behavior: behavior,
+            healthStatus: healthStatus,
+            isCastrated: isCastrated,
+            isInNeedOfSeeingVet: isInNeedOfSeeingVet,
+            additionalRequirements: additionalRequirements);
+        cat.PriorityScore = cat.CalculatePriorityScore(calculator);
+        return cat;
+    }
+    
+    public required string Name
+    {
+        get => _name;
+        set
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(value, nameof(Name));
+            if (value.Length > Constraints.NameMaxLength)
+            {
+                throw new ArgumentOutOfRangeException(nameof(Name), 
+                    $"Cat Name cannot be longer than {Constraints.NameMaxLength} characters.");
+            }
+            _name = value;   
+        }
+    }
+
+    public string? AdditionalRequirements
+    {
+        get => _additionalRequirements;
+        set
+        {
+            if (value?.Length > Constraints.AdditionalRequirementsMaxLength)
+            {
+                throw new ArgumentOutOfRangeException(nameof(AdditionalRequirements), 
+                    $"Cat Additional requirements cannot be longer than {Constraints.AdditionalRequirementsMaxLength} characters.");
+            }
+            _additionalRequirements = value;
+        }
+    }
+
     public required bool IsCastrated { get; set; }
     public required bool IsInNeedOfSeeingVet { get; set; }
     public required MedicalHelpUrgency MedicalHelpUrgency { get; set; }
@@ -18,66 +99,41 @@ public sealed class Cat : AuditableEntity
     public required Behavior Behavior { get; set; }
     public required HealthStatus HealthStatus { get; set; }
     
-    public required Guid PersonId { get; init; }
+    public Guid PersonId { get; private init; }
     public Person Person { get; private set; } = null!;
-    
-    public const int NameMaxLength = 100;
-    public const int AdditionalRequirementsMaxLength = 1000;
 
-    public double GetPriorityScore(int daysAwaiting, ICatPriorityCalculator calculator)
+
+    public double PriorityScore { get; private set; }
+    private double CalculatePriorityScore(ICatPriorityCalculator calculator)
     {
-        double priority = calculator.Calculate(this, daysAwaiting);
+        double priority = calculator.Calculate(this);
         return priority;
     }
-    
-    public sealed class CatNotFoundException(Guid id) : NotFoundException("Cat.NotFound", id.ToString());
+    public void ReCalculatePriorityScore(ICatPriorityCalculator calculator)
+    {
+        PriorityScore = CalculatePriorityScore(calculator);
+    }
+    public static class Constraints
+    {
+        public const int NameMaxLength = 100;
+        public const int AdditionalRequirementsMaxLength = 1000;
+    }
 }
 
 internal sealed class CatConfiguration : IEntityTypeConfiguration<Cat>
 {
     public void Configure(EntityTypeBuilder<Cat> builder)
     {
+        builder.ToTable("Cats");
+        builder.Property(x => x.PersonId).IsRequired();
         builder.Property(x => x.Name)
-            .HasMaxLength(Cat.NameMaxLength)
+            .HasMaxLength(Cat.Constraints.NameMaxLength)
             .IsRequired();
         builder.Property(x => x.AdditionalRequirements)
-            .HasMaxLength(Cat.AdditionalRequirementsMaxLength);
-        builder.Property(x => x.IsCastrated).IsRequired();
-        builder.Property(x => x.IsInNeedOfSeeingVet).IsRequired();
-        builder.Property(x => x.PersonId).IsRequired();
+            .HasMaxLength(Cat.Constraints.AdditionalRequirementsMaxLength);
         builder.Property(x => x.AgeCategory).IsRequired();
         builder.Property(x => x.Behavior).IsRequired();
         builder.Property(x => x.HealthStatus).IsRequired();
-    }
-}
-
-public interface ICatPriorityCalculator
-{
-    public double Calculate(Cat cat, int daysAwaiting);
-}
-
-internal class DefaultCatPriorityCalculator : ICatPriorityCalculator
-{
-    public double Calculate(Cat cat, int daysAwaiting)
-    {
-        const double healthWeight = 2.0;
-        const double medicalHelpUrgencyWeight = 2.0;
-        const double behaviorWeight = 1.5;
-        const double ageWeight = 1.2;
-        const double daysAwaitingWeight = 0.5;
-    
-        int healthStatusPoints = cat.HealthStatus.MaxScorePoints - cat.HealthStatus.ScorePoints;
-        int behaviourPoints = cat.Behavior.MaxScorePoints - cat.Behavior.ScorePoints;
-        int medicalHelpUrgencyPoints =  cat.MedicalHelpUrgency.MaxScorePoints - cat.MedicalHelpUrgency.ScorePoints;
-        int ageCategoryPoints = cat.AgeCategory.MaxScorePoints - cat.AgeCategory.ScorePoints;
-        
-        double priority = 
-            healthStatusPoints * healthWeight
-            + medicalHelpUrgencyPoints * medicalHelpUrgencyWeight
-            + behaviourPoints * behaviorWeight
-            + ageCategoryPoints * ageWeight
-            + daysAwaiting * daysAwaitingWeight;
-        
-        return priority;
+        builder.Property(x => x.PriorityScore).IsRequired();
     }
 }
