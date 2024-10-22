9ef20aeefd7befdd10c2971d2092f5cdb225af18
koniecdev <koniecdev@gmail.com>
2024-10-22 18:34:16 +0200

    tests cleanup service added

---
+++ tests/KittySaver.Api.Tests.Integration/Helpers/CleanupService.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Http.Json;
+using KittySaver.Api.Features.Persons.SharedContracts;
+
+namespace KittySaver.Api.Tests.Integration.Helpers;
+
+public class CleanupHelper
+{
+    private readonly HttpClient _httpClient;
+
+    public CleanupHelper(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task Cleanup()
+    {
+        await CleanupPersons();
+    }
+
+    private async Task CleanupPersons()
+    {
+        ICollection<PersonResponse>? persons = await _httpClient.GetFromJsonAsync<ICollection<PersonResponse>>("api/v1/persons");
+        if (persons is null)
+        {
+            return;
+        }
+        foreach (PersonResponse person in persons)
+        {
+            await _httpClient.DeleteAsync($"api/v1/persons/{person.Id}");
+        }
+    }
+}
