4f5da892f1a12b6586b53fb453236d028a33a5a9
koniecdev <koniecdev@gmail.com>
2024-10-22 13:31:23 +0200

    NotFoundExceptions file added

---
+++ src/KittySaver.Api/Shared/Exceptions/ApplicationExceptions/NotFoundExceptions.cs
@@ -0,0 +1,9 @@
+﻿using KittySaver.Api.Shared.Domain.Entites;
+
+namespace KittySaver.Api.Shared.Exceptions.ApplicationExceptions;
+
+public static class NotFoundExceptions
+{
+    public sealed class PersonNotFoundException(Guid id) : NotFoundException(nameof(Person), id.ToString());
+    public sealed class CatNotFoundException(Guid id) : NotFoundException(nameof(Cat), id.ToString());
+}
