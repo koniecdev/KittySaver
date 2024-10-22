378514e706ad75c88de4b987bfa380c613a349d7
koniecdev <koniecdev@gmail.com>
2024-10-22 15:42:15 +0200

    useless code removed

--- src/KittySaver.Api/Features/Persons/SharedContracts/ValidationPatterns.cs
+++ src/KittySaver.Api/Features/Persons/SharedContracts/ValidationPatterns.cs
@@ -1,6 +1,1 @@
 ﻿namespace KittySaver.Api.Features.Persons.SharedContracts;
-
-public static class ValidationPatterns
-{
-    public const string EmailPattern = @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$";
-}
