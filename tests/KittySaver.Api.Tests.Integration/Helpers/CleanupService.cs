3775a07c97355cd22facd86b7719547f82e4d5a5
koniecdev <koniecdev@gmail.com>
2024-10-22 22:27:35 +0200

    cleanup refactored to primary constructor

--- tests/KittySaver.Api.Tests.Integration/Helpers/CleanupService.cs
+++ tests/KittySaver.Api.Tests.Integration/Helpers/CleanupService.cs
@@ -3,15 +3,8 @@ using KittySaver.Api.Features.Persons.SharedContracts;
 
 namespace KittySaver.Api.Tests.Integration.Helpers;
 
-public class CleanupHelper
+public class CleanupHelper(HttpClient httpClient)
 {
-    private readonly HttpClient _httpClient;
-
-    public CleanupHelper(HttpClient httpClient)
-    {
-        _httpClient = httpClient;
-    }
-
     public async Task Cleanup()
     {
         await CleanupPersons();
@@ -19,14 +12,14 @@ public class CleanupHelper
 
     private async Task CleanupPersons()
     {
-        ICollection<PersonResponse>? persons = await _httpClient.GetFromJsonAsync<ICollection<PersonResponse>>("api/v1/persons");
+        ICollection<PersonResponse>? persons = await httpClient.GetFromJsonAsync<ICollection<PersonResponse>>("api/v1/persons");
         if (persons is null)
         {
             return;
         }
         foreach (PersonResponse person in persons)
         {
-            await _httpClient.DeleteAsync($"api/v1/persons/{person.Id}");
+            await httpClient.DeleteAsync($"api/v1/persons/{person.Id}");
         }
     }
 }
