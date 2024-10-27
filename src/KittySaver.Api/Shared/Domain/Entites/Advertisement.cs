6d32c6678ff23a699611933ae5c5de6ff1b0b144
koniecdev <koniecdev@gmail.com>
2024-10-27 22:18:03 +0100

    advertisement updated

--- src/KittySaver.Api/Shared/Domain/Entites/Advertisement.cs
+++ src/KittySaver.Api/Shared/Domain/Entites/Advertisement.cs
@@ -1,4 +1,5 @@
-﻿using KittySaver.Api.Shared.Domain.Entites.Common;
+﻿using System.Diagnostics.CodeAnalysis;
+using KittySaver.Api.Shared.Domain.Entites.Common;
 using KittySaver.Api.Shared.Domain.ValueObjects;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
@@ -7,15 +8,104 @@ namespace KittySaver.Api.Shared.Domain.Entites;
 
 public sealed class Advertisement : AuditableEntity
 {
+    public Advertisement Create(
+        Person person,
+        IEnumerable<Cat> cats,
+        PickupAddress? pickupAddress,
+        ContactInfo? contactInfo,
+        string? description,
+        DateTimeOffset createdOn)
+    {
+        List<Cat> catsToAssign = SetCats(person, cats);
+
+        PickupAddress pickupAddressToAssign =
+            pickupAddress ?? new PickupAddress(
+                person.Address.Country,
+                person.Address.State,
+                person.Address.ZipCode,
+                person.Address.City,
+                person.Address.Street,
+                person.Address.BuildingNumber);
+        
+        ContactInfo contactInfoToAssign =
+            contactInfo ?? new ContactInfo(
+                person.Email,
+                person.PhoneNumber);
+
+        DateTimeOffset expiresOn = CreatedOn.AddDays(30);
+
+        double catsMaximumPriorityScore = catsToAssign.Max(x => x.PriorityScore);
+        
+        Advertisement advertisement = new(
+            personId: person.Id,
+            cats: catsToAssign,
+            pickupAddress: pickupAddressToAssign,
+            contactInfo: contactInfoToAssign,
+            description: description,
+            createdOn: createdOn,
+            expiresOn: expiresOn,
+            priorityScore: catsMaximumPriorityScore);
+        
+        return advertisement;
+    }
+
+    private static List<Cat> SetCats(Person person, IEnumerable<Cat> cats)
+    {
+        List<Cat> catsToAssign = cats.ToList();
+        if (catsToAssign.Count == 0)
+        {
+            throw new ArgumentException("Advertisement cats list must not be empty.", nameof(cats));
+        }
+        
+        if (!catsToAssign.All(cat => person.Cats.Contains(cat)))
+        {
+            throw new ArgumentException("All provided cats for advertisement must be owned by single person.", nameof(cats));
+        }
+
+        if (!catsToAssign.Any(cat => cat.AdvertisementId is not null))
+        {
+            return catsToAssign;
+        }
+        
+        List<Cat> catsThatAreAlreadyAssigned = catsToAssign
+            .Where(cat => cat.AdvertisementId is not null)
+            .ToList();
+        
+        throw new ArgumentException(
+            $"Cats: {string.Join(",", catsThatAreAlreadyAssigned.Select(x=>x.Id))} are already assigned to another advertisement",
+            nameof(cats));
+    }
+
+    [SetsRequiredMembers]
+    private Advertisement(
+        Guid personId,
+        IEnumerable<Cat> cats,
+        PickupAddress pickupAddress,
+        ContactInfo contactInfo,
+        string? description,
+        DateTimeOffset createdOn,
+        DateTimeOffset expiresOn,
+        double priorityScore)
+    {
+        PersonId = personId;
+        _cats = cats.ToList();
+        PickupAddress = pickupAddress;
+        ContactInfo = contactInfo;
+        Description = description;
+        CreatedOn = createdOn;
+        ExpiresOn = expiresOn;
+    }
+    
     private readonly Guid _personId;
     
-    private List<Cat> _cats = [];
+    private List<Cat> _cats;
     
-    public DateTimeOffset? ActivatedOn { get; private set; }
+    public DateTimeOffset? ClosedOn { get; private set; }
+    public DateTimeOffset ExpiresOn { get; private set; }
     
-    public DateTimeOffset? ExpiresOn { get; private set; }
-    
-    public AdvertisementStatus Status { get; private set; } = AdvertisementStatus.Draft;
+    public AdvertisementStatus Status { get; private set; } = AdvertisementStatus.Active;
+
+    public double PriorityScore { get; private set; }
     
     public string? Description { get; set; }
     
@@ -33,12 +123,57 @@ public sealed class Advertisement : AuditableEntity
     }
     
     public Person Person { get; private set; } = null!;
-    
+
     public IReadOnlyList<Cat> Cats => _cats.ToList();
     
     public required PickupAddress PickupAddress { get; set; }
     
     public required ContactInfo ContactInfo { get; set; }
+
+    public void Close(DateTimeOffset currentDate)
+    {
+        if (Status is not AdvertisementStatus.Active)
+        {
+            throw new InvalidOperationException("You can not close advertisement that is not active");
+        }
+
+        foreach (Cat cat in _cats)
+        {
+            cat.MarkAsAdopted();
+        }
+
+        ClosedOn = currentDate;
+        Status = AdvertisementStatus.Closed;
+    }
+    
+    public void Expire(DateTimeOffset currentDate)
+    {
+        if (Status is not AdvertisementStatus.Active)
+        {
+            throw new InvalidOperationException("You can not expire advertisement that is not active");
+        }
+
+        if (ExpiresOn <= currentDate)
+        {
+            Status = AdvertisementStatus.Expired;
+        }
+    }
+
+    public void Refresh(DateTimeOffset currentDate)
+    {
+        if (Status is not (AdvertisementStatus.Active or AdvertisementStatus.Expired))
+        {
+            throw new InvalidOperationException("You can not refresh advertisement that is not active/expired");
+        }
+
+        ExpiresOn = currentDate.AddDays(30);
+    }
+
+    public void ReCalculatePriorityScore()
+    {
+        double catsMaximumPriorityScore = _cats.Max(x => x.PriorityScore);
+        PriorityScore = catsMaximumPriorityScore;
+    }
     
     public static class Constraints
     {
@@ -47,11 +182,9 @@ public sealed class Advertisement : AuditableEntity
     
     public enum AdvertisementStatus
     {
-        Draft,
         Active,
-        Paused,
         Closed,
-        Expired
+        Expired //TODO: Background running task every day
     }
 }
 
