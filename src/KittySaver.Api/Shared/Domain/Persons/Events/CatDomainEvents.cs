5127ec473f62e187c8d4579d0d5a15bfb25f058f
koniecdev <koniecdev@gmail.com>
2024-11-17 18:45:42 +0100

    Cats related domain events created

---
+++ src/KittySaver.Api/Shared/Domain/Persons/Events/CatDomainEvents.cs
@@ -0,0 +1,5 @@
+﻿using KittySaver.Api.Shared.Domain.Common.Primitives;
+
+namespace KittySaver.Api.Shared.Domain.Persons.Events;
+
+public record AssignedToAdvertisementCatStatusChangedDomainEvent(Guid AdvertisementId) : DomainEvent;
