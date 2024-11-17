bcced1addd0db1471004d2d018ef0240a0cfe7d3
koniecdev <koniecdev@gmail.com>
2024-11-17 18:44:53 +0100

    Person domain events created

---
+++ src/KittySaver.Api/Shared/Domain/Persons/Events/PersonDomainEvents.cs
@@ -0,0 +1,6 @@
+﻿using KittySaver.Api.Shared.Domain.Common.Primitives;
+
+namespace KittySaver.Api.Shared.Domain.Persons.Events;
+
+public record PersonDeletedDomainEvent(Guid PersonId) : DomainEvent;
+
