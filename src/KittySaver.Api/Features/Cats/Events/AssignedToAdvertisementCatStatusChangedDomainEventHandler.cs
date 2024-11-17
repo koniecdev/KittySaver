e01253513b71ee206d9f2c2ac6996eac7661db90
koniecdev <koniecdev@gmail.com>
2024-11-17 18:49:56 +0100

    Person aggregate EventHandlers created.

---
+++ src/KittySaver.Api/Features/Cats/Events/AssignedToAdvertisementCatStatusChangedDomainEventHandler.cs
@@ -0,0 +1,28 @@
+﻿using KittySaver.Api.Shared.Domain.Advertisement;
+using KittySaver.Api.Shared.Domain.Persons;
+using KittySaver.Api.Shared.Domain.Persons.Events;
+using KittySaver.Api.Shared.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace KittySaver.Api.Features.Cats.Events;
+
+public class AssignedToAdvertisementCatStatusChangedDomainEventHandler(ApplicationDbContext db)
+    : INotificationHandler<AssignedToAdvertisementCatStatusChangedDomainEvent>
+{
+    public async Task Handle(AssignedToAdvertisementCatStatusChangedDomainEvent notification, CancellationToken cancellationToken)
+    {
+        Advertisement advertisement =
+            await db.Advertisements
+                .FirstOrDefaultAsync(x => x.Id == notification.AdvertisementId, cancellationToken)
+            ?? throw new NotFoundExceptions.AdvertisementNotFoundException(notification.AdvertisementId);
+
+        Person person = await db.Persons
+            .Where(x => x.Id == advertisement.PersonId)
+            .Include(x => x.Cats)
+            .FirstAsync(cancellationToken);
+        
+        AdvertisementService advertisementService = AdvertisementService.Create(advertisement, person);
+        advertisementService.RecalculatePriorityScore();
+    }
+}
