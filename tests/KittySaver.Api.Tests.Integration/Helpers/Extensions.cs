556ae42520654bb05e76380c51fb3e9ce8dbab98
koniecdev <koniecdev@gmail.com>
2024-11-17 22:27:24 +0100

    Helpful string extension method added to test project

---
+++ tests/KittySaver.Api.Tests.Integration/Helpers/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace KittySaver.Api.Tests.Integration.Helpers;
+
+public static class Extensions
+{
+    public static string InsertSpacesIntoCamelCase(string input)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+            return input;
+
+        var result = new StringBuilder();
+        foreach (var character in input)
+        {
+            if (char.IsUpper(character) && result.Length > 0)
+            {
+                result.Append(' ');
+            }
+            result.Append(character);
+        }
+        return result.ToString();
+    }
+}
