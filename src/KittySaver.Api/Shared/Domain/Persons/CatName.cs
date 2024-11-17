6b3aa9ff2165070ac26bd4740440be531b743902
koniecdev <koniecdev@gmail.com>
2024-11-17 18:41:52 +0100

    CatName value object extracted

---
+++ src/KittySaver.Api/Shared/Domain/Persons/CatName.cs
@@ -0,0 +1,31 @@
+﻿using KittySaver.Api.Shared.Domain.Common.Primitives;
+
+namespace KittySaver.Api.Shared.Domain.Persons;
+
+public class CatName : ValueObject
+{
+    public const int MaxLength = 30;
+    private CatName(string value) => Value = value;
+    public string Value { get; }
+    public override string ToString() => Value;
+    public static implicit operator string(CatName catName) => catName.Value;
+
+    public static CatName Create(string catName)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(catName, nameof(catName));
+        if (catName.Length > MaxLength)
+        {
+            throw new ArgumentOutOfRangeException(nameof(catName), catName,
+                $"Maximum allowed length is: {MaxLength}");
+        }
+
+        string firstChar = catName[0].ToString().ToUpper();
+        string rest = catName[1..];
+        var catNameInstance = new CatName($"{firstChar}{rest}");
+        return catNameInstance;
+    }
+    protected override IEnumerable<object> GetAtomicValues()
+    {
+        yield return Value;
+    }
+}
