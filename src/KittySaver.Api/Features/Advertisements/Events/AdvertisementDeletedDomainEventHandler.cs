cda5e3264fd579dbb697318209e2a22b02eb12a6
koniecdev <koniecdev@gmail.com>
2024-11-24 20:02:02 +0100

    AdvertisementDeletedDomainEventHandler created

---
+++ src/KittySaver.Api/Features/Advertisements/Events/AdvertisementDeletedDomainEventHandler.cs
@@ -0,0 +1,23 @@
+﻿using KittySaver.Api.Shared.Domain.Advertisements;
+using KittySaver.Api.Shared.Domain.Advertisements.Events;
+using KittySaver.Api.Shared.Domain.Persons;
+using KittySaver.Api.Shared.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace KittySaver.Api.Features.Advertisements.Events;
+
+public class AdvertisementDeletedDomainEventHandler(ApplicationDbContext db)
+    : INotificationHandler<AdvertisementDeletedDomainEvent>
+{
+    public async Task Handle(AdvertisementDeletedDomainEvent notification, CancellationToken cancellationToken)
+    {
+        Person person =
+            await db.Persons
+                .Where(x => x.Id == notification.OwnerPersonId)
+                .Include(x => x.Cats)
+                .FirstAsync(cancellationToken);
+
+        person.UnassignCatsFromRemovedAdvertisement(notification.AdvertisementId);
+    }
+}
