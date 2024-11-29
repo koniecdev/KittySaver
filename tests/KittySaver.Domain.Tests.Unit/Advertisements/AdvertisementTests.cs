7540b4990beccbcacc074d09566cc0fcb6569451
koniecdev <koniecdev@gmail.com>
2024-11-29 23:46:47 +0100

    Advertisements unit tests updated.

--- tests/KittySaver.Domain.Tests.Unit/Advertisements/AdvertisementTests.cs
+++ tests/KittySaver.Domain.Tests.Unit/Advertisements/AdvertisementTests.cs
@@ -1,6 +1,8 @@
 ﻿using Bogus;
 using FluentAssertions;
 using KittySaver.Domain.Advertisements;
+using KittySaver.Domain.Advertisements.Events;
+using KittySaver.Domain.Common.Primitives;
 using KittySaver.Domain.Common.Primitives.Enums;
 using KittySaver.Domain.Persons;
 using KittySaver.Domain.ValueObjects;
@@ -100,6 +102,7 @@ public class AdvertisementTests
         advertisement.Description.Value.Should().Be("Lorem ipsum");
         advertisement.Status.Should().Be(Advertisement.AdvertisementStatus.Active);
         advertisement.PersonId.Should().Be(Person.Id);
+        advertisement.PriorityScore.Should().NotBe(0);
     }
 
     [Fact]
@@ -149,6 +152,33 @@ public class AdvertisementTests
         //Assert
         advertisement.Status.Should().Be(Advertisement.AdvertisementStatus.Closed);
         advertisement.ClosedOn.Should().Be(closureDate);
+        List<DomainEvent> events = advertisement.GetDomainEvents().ToList();
+        events.Count.Should().Be(1);
+        events.First().Should().BeOfType<AdvertisementClosedDomainEvent>();
+        AdvertisementClosedDomainEvent advertisementClosedDomainEvent = (AdvertisementClosedDomainEvent)events.First();
+        advertisementClosedDomainEvent.AdvertisementId.Should().Be(advertisement.Id);
+    }
+    
+    [Fact]
+    public void Close_ShouldThrowInvalidOperationException_WhenNotActiveAdvertisementIsProvided()
+    {
+        //Arrange
+        Advertisement advertisement = Advertisement.Create(
+            currentDate: Date,
+            person: Person,
+            catsIdsToAssign: [CatGenerator.Generate().Id],
+            pickupAddress: PickupAddressGenerator.Generate(),
+            contactInfoEmail: ContactInfoEmailGenerator.Generate(),
+            contactInfoPhoneNumber: ContactInfoPhoneNumberGenerator.Generate(),
+            description: Description.Create("lorem ipsum"));
+        advertisement.Expire(Date.AddDays(60));
+        
+        //Act
+        DateTimeOffset closureDate = Date.AddDays(62);
+        Action action = () => advertisement.Close(closureDate);
+
+        //Assert
+        action.Should().ThrowExactly<InvalidOperationException>();
     }
     
     [Fact]
@@ -173,4 +203,172 @@ public class AdvertisementTests
         //Assert
         advertisement.Status.Should().Be(Advertisement.AdvertisementStatus.Expired);
     }
+    
+    [Fact]
+    public void Expire_ShouldThrowInvalidOperationException_WhenNotActiveAdvertisementIsProvided()
+    {
+        //Arrange
+        Advertisement advertisement = Advertisement.Create(
+            currentDate: Date,
+            person: Person,
+            catsIdsToAssign: [CatGenerator.Generate().Id],
+            pickupAddress: PickupAddressGenerator.Generate(),
+            contactInfoEmail: ContactInfoEmailGenerator.Generate(),
+            contactInfoPhoneNumber: ContactInfoPhoneNumberGenerator.Generate(),
+            description: Description.Create("lorem ipsum"));
+        advertisement.Expire(Date.AddDays(60));
+        
+        //Act
+        DateTimeOffset expireDate = Date.AddDays(62);
+        Action action = () => advertisement.Expire(expireDate);
+
+        //Assert
+        action.Should().ThrowExactly<InvalidOperationException>();
+    }
+    
+    [Fact]
+    public void Refresh_ShouldExpireSuccessfully_WhenValidDataAreProvided()
+    {
+        //Arrange
+        Cat cat = CatGenerator.Generate();
+
+        Advertisement advertisement = Advertisement.Create(
+            currentDate: Date,
+            person: Person,
+            catsIdsToAssign: [cat.Id],
+            pickupAddress: PickupAddressGenerator,
+            contactInfoEmail: ContactInfoEmailGenerator.Generate(),
+            contactInfoPhoneNumber: ContactInfoPhoneNumberGenerator.Generate(),
+            description: Description.Create("lorem ipsum"));
+        
+        DateTimeOffset expirationDate = advertisement.ExpiresOn.AddDays(1);
+        advertisement.Expire(expirationDate);
+        
+        //Act
+        DateTimeOffset refreshDate = expirationDate.AddDays(2);
+        advertisement.Refresh(refreshDate);
+        
+        //Assert
+        advertisement.Status.Should().Be(Advertisement.AdvertisementStatus.Active);
+    }
+    
+    [Fact]
+    public void Refresh_ShouldThrowInvalidOperationException_WhenNotActiveAdvertisementIsProvided()
+    {
+        //Arrange
+        Advertisement advertisement = Advertisement.Create(
+            currentDate: Date,
+            person: Person,
+            catsIdsToAssign: [CatGenerator.Generate().Id],
+            pickupAddress: PickupAddressGenerator.Generate(),
+            contactInfoEmail: ContactInfoEmailGenerator.Generate(),
+            contactInfoPhoneNumber: ContactInfoPhoneNumberGenerator.Generate(),
+            description: Description.Create("lorem ipsum"));
+        DateTimeOffset closureDate = advertisement.ExpiresOn.AddDays(1);
+        advertisement.Close(closureDate);
+        
+        //Act
+        DateTimeOffset refreshDate = closureDate.AddDays(2);
+        Action action = () => advertisement.Refresh(refreshDate);
+
+        //Assert
+        action.Should().ThrowExactly<InvalidOperationException>();
+    }
+    
+    [Fact]
+    public void ValidateOwnership_ShouldBeSuccessfull_WhenValidDataAreProvided()
+    {
+        //Arrange
+        Cat cat = CatGenerator.Generate();
+
+        Advertisement advertisement = Advertisement.Create(
+            currentDate: Date,
+            person: Person,
+            catsIdsToAssign: [cat.Id],
+            pickupAddress: PickupAddressGenerator,
+            contactInfoEmail: ContactInfoEmailGenerator.Generate(),
+            contactInfoPhoneNumber: ContactInfoPhoneNumberGenerator.Generate(),
+            description: Description.Create("lorem ipsum"));
+        
+        //Act
+        Action validation = () => advertisement.ValidateOwnership(Person.Id);
+        
+        //Assert
+        validation.Should().NotThrow();
+    }
+    
+    [Fact]
+    public void ValidateOwnership_ShouldThrowArgumentException_WhenPersonDifferentThanOwnerIsProvided()
+    {
+        //Arrange
+        Advertisement advertisement = Advertisement.Create(
+            currentDate: Date,
+            person: Person,
+            catsIdsToAssign: [CatGenerator.Generate().Id],
+            pickupAddress: PickupAddressGenerator.Generate(),
+            contactInfoEmail: ContactInfoEmailGenerator.Generate(),
+            contactInfoPhoneNumber: ContactInfoPhoneNumberGenerator.Generate(),
+            description: Description.Create("lorem ipsum"));
+        DateTimeOffset closureDate = advertisement.ExpiresOn.AddDays(1);
+        advertisement.Close(closureDate);
+        
+        //Act
+        Action validation = () => advertisement.ValidateOwnership(Guid.NewGuid());
+
+        //Assert
+        validation.Should().ThrowExactly<ArgumentException>();
+    }
+    
+    [Fact]
+    public void PersonIdSet_ShouldThrowArgumentException_WhenProvidedEmptyValue()
+    {
+        Person person = new Faker<Person>()
+        .CustomInstantiator(faker =>
+            Person.Create(
+                userIdentityId: Guid.NewGuid(),
+                firstName: FirstName.Create(faker.Person.FirstName),
+                lastName: LastName.Create(faker.Person.LastName),
+                email: Email.Create(faker.Person.Email),
+                phoneNumber: PhoneNumber.Create(faker.Person.Phone),
+                residentalAddress: Address,
+                defaultAdvertisementPickupAddress: Address,
+                defaultAdvertisementContactInfoEmail: Email.Create(faker.Person.Email),
+                defaultAdvertisementContactInfoPhoneNumber: PhoneNumber.Create(faker.Person.Phone)
+            )).Generate();
+
+        Cat cat = new Faker<Cat>()
+            .CustomInstantiator(faker =>
+                Cat.Create(
+                    priorityScoreCalculator: new DefaultCatPriorityCalculatorService(),
+                    person: person,
+                    name: CatName.Create(faker.Person.FirstName),
+                    medicalHelpUrgency: faker.PickRandomParam(MedicalHelpUrgency.NoNeed,
+                        MedicalHelpUrgency.ShouldSeeVet,
+                        MedicalHelpUrgency.HaveToSeeVet),
+                    ageCategory: faker.PickRandomParam(AgeCategory.Baby, AgeCategory.Adult, AgeCategory.Senior),
+                    behavior: faker.PickRandomParam(Behavior.Unfriendly, Behavior.Friendly),
+                    healthStatus: faker.PickRandomParam(HealthStatus.Critical, HealthStatus.Poor, HealthStatus.Good),
+                    isCastrated: faker.PickRandomParam(true, false),
+                    additionalRequirements: Description.Create(faker.Lorem.Lines(2))
+                )).Generate();
+        
+        SharedHelper.SetBackingField(person, nameof(Person.Id), Guid.Empty);
+        
+        //Act
+        Action createAdvertisement = () =>
+        {
+            Advertisement.Create(
+                currentDate: new DateTimeOffset(2024, 1, 1, 1, 1, 1, TimeSpan.Zero),
+                person: person,
+                catsIdsToAssign: [cat.Id],
+                pickupAddress: person.DefaultAdvertisementsPickupAddress,
+                contactInfoEmail: person.DefaultAdvertisementsContactInfoEmail,
+                contactInfoPhoneNumber: person.DefaultAdvertisementsContactInfoPhoneNumber,
+                description: Description.Create("Lorem ipsum"));
+        };
+        
+        //Assert
+        createAdvertisement.Should().Throw<ArgumentException>().WithMessage("Provided person id is empty. (Parameter 'PersonId')");
+    }
+    
 }
