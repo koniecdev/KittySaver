99f254b9c7f2a71bce1e7a9b5a96d4043f406a5b
koniecdev <koniecdev@gmail.com>
2024-12-03 14:30:00 +0100

    AdvertisementRepository updated.

--- src/KittySaver.Api/Features/Advertisements/SharedContracts/AdvertisementRepository.cs
+++ src/KittySaver.Api/Features/Advertisements/SharedContracts/AdvertisementRepository.cs
@@ -1,17 +1,23 @@
-﻿using KittySaver.Domain.Advertisements;
+﻿using KittySaver.Api.Shared.Persistence;
+using KittySaver.Domain.Advertisements;
 using KittySaver.Domain.Common.Exceptions;
+using KittySaver.Domain.Persons;
 using Microsoft.EntityFrameworkCore;
 
-namespace KittySaver.Api.Shared.Persistence.Repositories;
+namespace KittySaver.Api.Features.Advertisements.SharedContracts;
 
 public class AdvertisementRepository(ApplicationDbContext db) : IAdvertisementRepository
 {
-    public async Task<IEnumerable<Advertisement>> GetAllAdvertisementsAsync()
+    public async Task<Advertisement> GetAdvertisementByIdAsync(Guid id, CancellationToken cancellationToken)
         => await db.Advertisements
-            .ToListAsync();
-
-    public async Task<Advertisement> GetAdvertisementByIdAsync(Guid id)
-        => await db.Advertisements
-               .FirstOrDefaultAsync(advertisement => advertisement.Id == id)
+               .FirstOrDefaultAsync(advertisement => advertisement.Id == id, cancellationToken)
                 ?? throw new NotFoundExceptions.AdvertisementNotFoundException(id);
+
+    public void Insert(Advertisement advertisement) => db.Advertisements.Add(advertisement);
+    
+    public void Remove(Advertisement advertisement)
+    {
+        db.Remove(advertisement);
+        advertisement.AnnounceDeletion();
+    }
 }
--- src/KittySaver.Domain/Advertisements/IAdvertisementRepository.cs
+++ src/KittySaver.Domain/Advertisements/IAdvertisementRepository.cs
@@ -1,7 +1,10 @@
-﻿namespace KittySaver.Domain.Advertisements;
+﻿using KittySaver.Domain.Persons;
+
+namespace KittySaver.Domain.Advertisements;
 
 public interface IAdvertisementRepository
 {
-    public Task<IEnumerable<Advertisement>> GetAllAdvertisementsAsync();
-    public Task<Advertisement> GetAdvertisementByIdAsync(Guid id);
+    public Task<Advertisement> GetAdvertisementByIdAsync(Guid id, CancellationToken cancellationToken);
+    public void Insert(Advertisement advertisement);
+    public void Remove(Advertisement advertisement);
 }
