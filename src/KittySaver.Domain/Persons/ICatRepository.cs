0d244ad8a87c8ce668e3d9ae3a146df4323bd9c5
koniecdev <koniecdev@gmail.com>
2024-12-03 14:12:00 +0100

    Moved files.

--- src/KittySaver.Domain/Persons/ICatRepository.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace KittySaver.Domain.Persons;
-
-public interface ICatRepository
-{
-    public IEnumerable<Cat> GetAllCats(Person owner);
-    public Cat GetCatById(Person owner, Guid id);
-}
