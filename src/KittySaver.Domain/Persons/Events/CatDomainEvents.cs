88415277d99843b8f38aa28b620fb3bd4b7af8a6
koniecdev <koniecdev@gmail.com>
2024-12-12 22:12:27 +0100

    Useless eventhandler and service removed

--- src/KittySaver.Domain/Persons/Events/CatDomainEvents.cs
+++
@@ -1,5 +0,0 @@
-﻿using KittySaver.Domain.Common.Primitives;
-
-namespace KittySaver.Domain.Persons.Events;
-
-public record AssignedToAdvertisementCatStatusChangedDomainEvent(Guid AdvertisementId) : DomainEvent;
