adee8ac98b9f1d2244f7a4d67cc4531e82a4ff59
koniecdev <koniecdev@gmail.com>
2024-12-12 20:21:45 +0100

    RemoveAdvertisement feature updated

--- src/KittySaver.Api/Features/Advertisements/DeleteAdvertisement.cs
+++ src/KittySaver.Api/Features/Advertisements/DeleteAdvertisement.cs
@@ -4,6 +4,7 @@ using KittySaver.Api.Shared.Infrastructure.ApiComponents;
 using KittySaver.Api.Shared.Persistence;
 using KittySaver.Domain.Advertisements;
 using KittySaver.Domain.Common.Exceptions;
+using KittySaver.Domain.Persons;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,37 +12,44 @@ namespace KittySaver.Api.Features.Advertisements;
 
 public sealed class DeleteAdvertisement : IEndpoint
 {
-    public sealed record DeleteAdvertisementCommand(Guid Id) : ICommand;
+    public sealed record DeleteAdvertisementCommand(Guid PersonId, Guid AdvertisementId) : ICommand;
 
     public sealed class DeleteAdvertisementCommandValidator
         : AbstractValidator<DeleteAdvertisementCommand>
     {
         public DeleteAdvertisementCommandValidator()
         {
-            RuleFor(x => x.Id).NotEmpty();
+            RuleFor(x => x.PersonId)
+                .NotEmpty()
+                .NotEqual(x => x.AdvertisementId);
+            RuleFor(x => x.AdvertisementId)
+                .NotEmpty()
+                .NotEqual(x => x.PersonId);
         }
     }
 
     internal sealed class DeleteAdvertisementCommandHandler(
-        IAdvertisementRepository advertisementRepository,
+        IPersonRepository personRepository,
         IUnitOfWork unitOfWork)
         : IRequestHandler<DeleteAdvertisementCommand>
     {
         public async Task Handle(DeleteAdvertisementCommand request, CancellationToken cancellationToken)
         {
-            Advertisement advertisement = await advertisementRepository.GetAdvertisementByIdAsync(request.Id, cancellationToken);
-            advertisementRepository.Remove(advertisement);
+            Person owner = await personRepository.GetPersonByIdAsync(request.PersonId, cancellationToken);
+            owner.RemoveAdvertisement(request.AdvertisementId);
             await unitOfWork.SaveChangesAsync(cancellationToken);
         }
     }
 
     public void MapEndpoint(IEndpointRouteBuilder endpointRouteBuilder)
     {
-        endpointRouteBuilder.MapDelete("advertisements/{id:guid}", async (Guid id,
+        endpointRouteBuilder.MapDelete("persons/{personId:guid}/advertisements/{advertisementId:guid}", async (
+            Guid personId,
+            Guid advertisementId,
             ISender sender,
             CancellationToken cancellationToken) =>
         {
-            DeleteAdvertisementCommand command = new(id);
+            DeleteAdvertisementCommand command = new(PersonId: personId, AdvertisementId: advertisementId);
             await sender.Send(command, cancellationToken);
             return Results.NoContent();
         }).RequireAuthorization();
