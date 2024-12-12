c2c2f6df3240701238760f5143167ef4974ed481
koniecdev <koniecdev@gmail.com>
2024-12-12 20:07:14 +0100

    Removed advertisement events

--- src/KittySaver.Domain/Advertisements/Events/AdvertisementDomainEvents.cs
+++
@@ -1,6 +0,0 @@
-﻿using KittySaver.Domain.Common.Primitives;
-
-namespace KittySaver.Domain.Advertisements.Events;
-
-public record AdvertisementDeletedDomainEvent(Guid AdvertisementId, Guid OwnerPersonId) : DomainEvent;
-public record AdvertisementClosedDomainEvent(Guid AdvertisementId) : DomainEvent;
