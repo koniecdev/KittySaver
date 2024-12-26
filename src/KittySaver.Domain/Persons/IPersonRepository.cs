b4040342b2bb769b4ca0bc148d7f6f21005e72c4
koniecdev <koniecdev@gmail.com>
2024-12-26 13:39:46 +0100

    New repository extracted for better SRP adhere

--- src/KittySaver.Domain/Persons/IPersonRepository.cs
+++ src/KittySaver.Domain/Persons/IPersonRepository.cs
@@ -4,9 +4,6 @@ public interface IPersonRepository
 {
     public Task<Person> GetPersonByIdAsync(Guid id, CancellationToken cancellationToken);
     public Task<Person> GetPersonByIdOrIdentityIdAsync(Guid idOrIdentityId, CancellationToken cancellationToken);
-    public Task<bool> IsPhoneNumberUniqueAsync(string phone, Guid? userToExcludeIdOrIdentityId, CancellationToken cancellationToken);
-    public Task<bool> IsEmailUniqueAsync(string email, Guid? userToExcludeIdOrIdentityId, CancellationToken cancellationToken);
-    public Task<bool> IsUserIdentityIdUniqueAsync(Guid userIdentityId, CancellationToken cancellationToken);
     public void Insert(Person person);
     public void Remove(Person person);
 }
