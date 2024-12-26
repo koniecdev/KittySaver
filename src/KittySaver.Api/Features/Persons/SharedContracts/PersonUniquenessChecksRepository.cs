cd38e7ed9a9b4ad4b10ce29fb0cf64ea7e43f89f
koniecdev <koniecdev@gmail.com>
2024-12-26 13:41:44 +0100

    New repository implementation added

---
+++ src/KittySaver.Api/Features/Persons/SharedContracts/PersonUniquenessChecksRepository.cs
@@ -0,0 +1,45 @@
+﻿using KittySaver.Api.Shared.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace KittySaver.Api.Features.Persons.SharedContracts;
+
+public interface IPersonUniquenessChecksRepository
+{
+    public Task<bool> IsPhoneNumberUniqueAsync(string phone, Guid? userToExcludeIdOrIdentityId, CancellationToken cancellationToken);
+    public Task<bool> IsEmailUniqueAsync(string email, Guid? userToExcludeIdOrIdentityId, CancellationToken cancellationToken);
+    public Task<bool> IsUserIdentityIdUniqueAsync(Guid userIdentityId, CancellationToken cancellationToken);
+}
+
+public class PersonUniquenessChecksRepository(ApplicationWriteDbContext writeDb) : IPersonUniquenessChecksRepository
+{
+    public async Task<bool> IsPhoneNumberUniqueAsync(string phone, Guid? userToExcludeIdOrIdentityId, CancellationToken cancellationToken) 
+        => userToExcludeIdOrIdentityId is null 
+            ? !await writeDb.Persons
+                .AsNoTracking()
+                .AnyAsync(person => person.PhoneNumber.Value == phone, cancellationToken)
+            : !await writeDb.Persons
+                .AsNoTracking()
+                .AnyAsync(x => 
+                        x.PhoneNumber.Value == phone 
+                        && x.Id != userToExcludeIdOrIdentityId 
+                        && x.UserIdentityId != userToExcludeIdOrIdentityId,
+                    cancellationToken);
+        
+    public async Task<bool> IsEmailUniqueAsync(string email, Guid? userToExcludeIdOrIdentityId, CancellationToken cancellationToken) 
+        => userToExcludeIdOrIdentityId is null 
+            ? !await writeDb.Persons
+                .AsNoTracking()
+                .AnyAsync(person => person.Email.Value == email, cancellationToken)
+            : !await writeDb.Persons
+                .AsNoTracking()
+                .AnyAsync(x =>
+                        x.Email.Value == email 
+                        && x.Id != userToExcludeIdOrIdentityId 
+                        && x.UserIdentityId != userToExcludeIdOrIdentityId,
+                    cancellationToken);
+
+    public async Task<bool> IsUserIdentityIdUniqueAsync(Guid userIdentityId, CancellationToken cancellationToken) 
+        => !await writeDb.Persons
+            .AsNoTracking()
+            .AnyAsync(person => person.UserIdentityId == userIdentityId, cancellationToken);
+}
