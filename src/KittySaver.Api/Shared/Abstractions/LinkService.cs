d2deea519de79f425ea4c81149bb038c12d0e660
koniecdev <koniecdev@gmail.com>
2025-01-05 00:38:48 +0100

    Hateoas responses handled better

--- src/KittySaver.Api/Shared/Abstractions/LinkService.cs
+++ src/KittySaver.Api/Shared/Abstractions/LinkService.cs
@@ -4,14 +4,6 @@ using KittySaver.Domain.Persons;
 
 namespace KittySaver.Api.Shared.Abstractions;
 
-public sealed class Link(string href, string rel, string method, bool templated = false)
-{
-    public string Href { get; } = href;
-    public string Rel { get; } = rel;
-    public string Method { get; } = method;
-    public bool Templated { get; } = templated;
-}
-
 public interface ILinkService
 {
     public List<Link> GeneratePersonRelatedLinks(Guid personId, CurrentlyLoggedInPerson? currentlyLoggedInPerson);
---
+++ src/KittySaver.Api/Shared/Contracts/HateoasResponse.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using KittySaver.Api.Shared.Abstractions;
+
+namespace KittySaver.Api.Shared.Contracts;
+
+public class HateoasResponse : IHateoasResponse
+{
+    public ICollection<Link> Links { get; set; } = new List<Link>();
+}
+
+public class PersonHateoasResponse(Guid id) : IHateoasPersonResponse
+{
+    public Guid Id { get; } = id;
+    public ICollection<Link> Links { get; set; } = new List<Link>();
+}
