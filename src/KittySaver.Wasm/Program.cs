b4000b269de66b93610c2adf76f539644130e7ca
koniecdev <koniecdev@gmail.com>
2025-01-12 01:01:26 +0100

    Blazor adjusted to handle hateoas

--- src/KittySaver.Wasm/Program.cs
+++ src/KittySaver.Wasm/Program.cs
@@ -2,15 +2,12 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using KittySaver.Wasm;
 using KittySaver.Wasm.Shared.HttpClients;
+using KittySaver.Wasm.Shared.HttpClients.MainApiResponses;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Configuration.AddInMemoryCollection(new Dictionary<string, string>
-{
-    {"ApiBaseUrl", "https://localhost:7127/"}
-}!);
+builder.Services.AddScoped<IApiNavigationService, ApiNavigationService>();
 builder.Services.AddApiClient(builder.Configuration);
 
 await builder.Build().RunAsync();
---
+++ src/KittySaver.Wasm/Shared/HttpClients/MainApiResponses/EndpointRels.cs
@@ -0,0 +1,40 @@
+﻿namespace KittySaver.Wasm.Shared.HttpClients.MainApiResponses;
+
+public static class EndpointRels
+{
+    public const string SelfRel = "self";
+
+    public static class Person
+    {
+        public const string CreatePersonRel = "create-person";
+        public const string UpdatePersonRel = "update-person";
+        public const string DeletePersonRel = "delete-person";
+        public const string GetPersonRel = "get-person";
+        public const string GetPersonsRel = "get-persons";
+    }
+
+    public static class Cat
+    {
+        public const string CreateCatRel = "create-cat";
+        public const string UpdateCatRel = "update-cat";
+        public const string DeleteCatRel = "delete-cat";
+        public const string GetCatRel = "get-cat";
+        public const string GetCatsRel = "get-cats";
+    }
+
+    public static class Advertisement
+    {
+        public const string CreateAdvertisementRel = "create-advertisement";
+        public const string UpdateAdvertisementRel = "update-advertisement";
+        public const string DeleteAdvertisementRel = "delete-advertisement";
+        public const string GetAdvertisementRel = "get-advertisement";
+        public const string GetAdvertisementsRel = "get-advertisements";
+        public const string GetPersonAdvertisementsRel = "get-advertisements-by-personid";
+        public const string CloseAdvertisementRel = "close-advertisement";
+        public const string ExpireAdvertisementsRel = "expire-advertisement";
+        public const string RefreshAdvertisementRel = "refresh-advertisement";
+        public const string ReassignCatsToAdvertisementRel = "reassign-cats-to-advertisement";
+        public const string UpdateAdvertisementThumbnailRel = "update-advertisement-thumbnail";
+        public const string GetAdvertisementThumbnailRel = "get-advertisement-thumbnail";
+    }
+}
