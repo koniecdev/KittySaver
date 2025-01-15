82315726063044fd4b44d9f32f7a17864c38ac7a
koniecdev <koniecdev@gmail.com>
2025-01-15 18:45:26 +0100

    DeleteAdvertisement feature adjusted

--- src/KittySaver.Api/Features/Advertisements/DeleteAdvertisement.cs
+++ src/KittySaver.Api/Features/Advertisements/DeleteAdvertisement.cs
@@ -27,13 +27,26 @@ public sealed class DeleteAdvertisement : IEndpoint
 
     internal sealed class DeleteAdvertisementCommandHandler(
         IPersonRepository personRepository,
-        IUnitOfWork unitOfWork)
+        IUnitOfWork unitOfWork,
+        IWebHostEnvironment webHostEnvironment)
         : IRequestHandler<DeleteAdvertisementCommand>
     {
+        private readonly string _advertisementsBasePath = Path.Combine(
+            webHostEnvironment.ContentRootPath,
+            "PrivateFiles",
+            "advertisements");
+        
         public async Task Handle(DeleteAdvertisementCommand request, CancellationToken cancellationToken)
         {
             Person owner = await personRepository.GetPersonByIdAsync(request.PersonId, cancellationToken);
             owner.RemoveAdvertisement(request.Id);
+            
+            string advertisementPath = Path.Combine(_advertisementsBasePath, request.Id.ToString());
+            if (Directory.Exists(advertisementPath))
+            {
+                Directory.Delete(advertisementPath, recursive: true);
+            }
+            
             await unitOfWork.SaveChangesAsync(cancellationToken);
         }
     }
