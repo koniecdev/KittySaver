c8949476f3d61cbf2be7a748bd2d8b2f39846363
koniecdev <koniecdev@gmail.com>
2025-01-15 05:55:48 +0100

    FileStorageService refactored

--- src/KittySaver.Api/Shared/Infrastructure/Services/FileStorageService.cs
+++ src/KittySaver.Api/Shared/Infrastructure/Services/FileStorageService.cs
@@ -2,77 +2,74 @@
 
 public interface IFileStorageService
 {
-    Task SaveFileAsync(Stream sourceStream, string fileName, CancellationToken cancellationToken);
-    Task<FileStream> GetFileAsync(string fileName, CancellationToken cancellationToken);
-    Task DeleteFileAsync(string fileName, CancellationToken cancellationToken);
+    Task SaveFileAsync(Stream sourceStream, string? subdirectoryPath, string filenameWithExtension, CancellationToken cancellationToken);
+    public FileStream GetFileStream(string filePath);
+    public FileStream GetFileSteam(string subdirectoryPath, string fileName);
+    void DeleteFile(string path);
 }
 
 public sealed class LocalFileStorageService(IWebHostEnvironment webHostEnvironment) : IFileStorageService
 {
     private readonly string _basePath = Path.Combine(
         webHostEnvironment.ContentRootPath, 
-        "PrivateFiles", 
-        "advertisements");
+        "PrivateFiles");
 
-    public async Task SaveFileAsync(Stream sourceStream, string fileName, CancellationToken cancellationToken)
+    public async Task SaveFileAsync(Stream sourceStream, string? subdirectoryPath, string filenameWithExtension, CancellationToken cancellationToken)
     {
-        string advertisementId = Path.GetFileNameWithoutExtension(fileName).Split('_')[0];
-        string thumbnailDirectory = Path.Combine(_basePath, advertisementId, "thumbnail");
-        
-        // Create directory if it doesn't exist
-        Directory.CreateDirectory(thumbnailDirectory);
-        
-        // Clean existing thumbnail (if any)
-        foreach (string existingFile in Directory.GetFiles(thumbnailDirectory))
+        ArgumentNullException.ThrowIfNull(sourceStream);
+        ArgumentException.ThrowIfNullOrEmpty(subdirectoryPath);
+        ArgumentException.ThrowIfNullOrEmpty(filenameWithExtension);
+
+        string fullDirectoryPath = Path.Combine(_basePath, subdirectoryPath);
+        if (!Directory.Exists(fullDirectoryPath))
         {
-            File.Delete(existingFile);
+            Directory.CreateDirectory(fullDirectoryPath);
         }
-        
-        // Save new thumbnail
-        string extension = Path.GetExtension(fileName);
-        string filePath = Path.Combine(thumbnailDirectory, $"thumbnail{extension}");
-        
-        await using FileStream destinationStream = File.Create(filePath);
+
+        string fullPath = Path.Combine(fullDirectoryPath, filenameWithExtension);
+
+        await using FileStream destinationStream = File.Create(fullPath);
         await sourceStream.CopyToAsync(destinationStream, cancellationToken);
     }
 
-    public Task<FileStream> GetFileAsync(string fileName, CancellationToken cancellationToken)
+    private string[] GetAllFilesPaths(string subdirectoryPath)
     {
-        string advertisementId = Path.GetFileNameWithoutExtension(fileName).Split('_')[0];
-        string thumbnailDirectory = Path.Combine(_basePath, advertisementId, "thumbnail");
-        
-        if (!Directory.Exists(thumbnailDirectory))
+        string directory = Path.Combine(_basePath, subdirectoryPath);
+        if (!Directory.Exists(directory))
         {
-            throw new FileNotFoundException($"Thumbnail for advertisement {advertisementId} not found");
+            throw new FileNotFoundException($"Required path ${subdirectoryPath} has not been found.");
         }
-
-        string[] files = Directory.GetFiles(thumbnailDirectory);
-        if (files.Length == 0)
-        {
-            throw new FileNotFoundException($"Thumbnail for advertisement {advertisementId} not found");
-        }
-
-        string thumbnailPath = files[0];
-        return Task.FromResult(new FileStream(thumbnailPath, FileMode.Open, FileAccess.Read));
+        string[] files = Directory.GetFiles(directory);
+        return files;
     }
+    
+    public FileStream GetFileStream(string filePath) => new(filePath, FileMode.Open, FileAccess.Read);
 
-    public Task DeleteFileAsync(string fileName, CancellationToken cancellationToken)
+    public FileStream GetFileSteam(string subdirectoryPath, string fileName)
     {
-        string advertisementId = Path.GetFileNameWithoutExtension(fileName).Split('-')[0];
-        string thumbnailDirectory = Path.Combine(_basePath, advertisementId, "thumbnail");
+        string fullPath = Path.Combine(_basePath, subdirectoryPath);
 
-        if (!Directory.Exists(thumbnailDirectory))
+        string[] files = GetAllFilesPaths(fullPath);
+        string? filePath = files.FirstOrDefault(x => x == fileName || x.Contains(fileName));
+        if (filePath is null)
         {
-            return Task.CompletedTask;
-        }
-        Directory.Delete(thumbnailDirectory, recursive: true);
-            
-        string advertisementDirectory = Path.GetDirectoryName(thumbnailDirectory)!;
-        if (!Directory.EnumerateFileSystemEntries(advertisementDirectory).Any())
-        {
-            Directory.Delete(advertisementDirectory);
+            throw new FileNotFoundException($"File with name that is equal or starts with {fileName} within {subdirectoryPath} has not been found");
         }
 
-        return Task.CompletedTask;
+        return new FileStream(filePath, FileMode.Open, FileAccess.Read);
+    }
+    
+    public void DeleteFile(string filePath)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(filePath);
+
+        string fullPath = Path.Combine(_basePath, filePath);
+        
+        if (!File.Exists(fullPath))
+        {
+            return;
+        }
+
+        File.Delete(fullPath);
     }
 }
