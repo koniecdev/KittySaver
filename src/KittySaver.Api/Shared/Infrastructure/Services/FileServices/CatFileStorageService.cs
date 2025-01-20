cb2b9573cb69641fa3e06ab75ae540712eb325f4
koniecdev <koniecdev@gmail.com>
2025-01-20 21:54:25 +0100

    CatFileStorageService created

---
+++ src/KittySaver.Api/Shared/Infrastructure/Services/FileServices/CatFileStorageService.cs
@@ -0,0 +1,29 @@
+﻿namespace KittySaver.Api.Shared.Infrastructure.Services.FileServices;
+
+public interface ICatFileStorageService
+{
+    Task SaveThumbnailAsync(IFormFile sourceFile, Guid advertisementId, CancellationToken cancellationToken);
+    FileStream GetThumbnail(Guid advertisementId);
+    string GetContentType(string fileName);
+    void DeleteThumbnail(Guid advertisementId);
+}
+
+public class CatFileStorageService(IThumbnailStorageService thumbnailStorage)
+    : ICatFileStorageService
+{
+    private const string EntityType = "cats";
+
+    public Task SaveThumbnailAsync(IFormFile sourceFile, Guid advertisementId, CancellationToken cancellationToken) =>
+        thumbnailStorage.SaveThumbnailAsync(sourceFile, EntityType, advertisementId, cancellationToken);
+
+    public FileStream GetThumbnail(Guid advertisementId) =>
+        thumbnailStorage.GetThumbnail(EntityType, advertisementId);
+        
+    public string GetContentType(string fileName) =>
+        thumbnailStorage.GetContentType(fileName);
+
+    public void DeleteThumbnail(Guid advertisementId)
+    {
+        thumbnailStorage.DeleteThumbnail(EntityType, advertisementId);
+    }
+}
