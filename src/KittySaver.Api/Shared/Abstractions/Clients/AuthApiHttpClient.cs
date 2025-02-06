8dab1a9c096278a0f32aa07c1c36122359ed75ec
koniecdev <koniecdev@gmail.com>
2025-02-06 21:40:08 +0100

    AuthApiClient changes

--- src/KittySaver.Api/Shared/Abstractions/Clients/AuthApiHttpClient.cs
+++ src/KittySaver.Api/Shared/Abstractions/Clients/AuthApiHttpClient.cs
@@ -23,78 +23,21 @@ public class ApiValidationException(ValidationProblemDetails problemDetails)
 public interface IAuthApiHttpClient
 {
     protected record IdResponse(Guid Id);
-    public sealed record PersonDto(Guid Id, string Email, string Username, string PhoneNumber);
     public sealed record RegisterDto(
         string UserName,
         string Email,
         string PhoneNumber,
-        string Password,
-        string DefaultAdvertisementPickupAddressCountry,
-        string? DefaultAdvertisementPickupAddressState,
-        string DefaultAdvertisementPickupAddressZipCode,
-        string DefaultAdvertisementPickupAddressCity,
-        string? DefaultAdvertisementPickupAddressStreet,
-        string? DefaultAdvertisementPickupAddressBuildingNumber,
-        string DefaultAdvertisementContactInfoEmail,
-        string DefaultAdvertisementContactInfoPhoneNumber);
-    public sealed record UpdatePersonDto(
-        string UserName,
-        string Email,
-        string PhoneNumber,
-        string DefaultAdvertisementPickupAddressCountry,
-        string? DefaultAdvertisementPickupAddressState,
-        string DefaultAdvertisementPickupAddressZipCode,
-        string DefaultAdvertisementPickupAddressCity,
-        string? DefaultAdvertisementPickupAddressStreet,
-        string? DefaultAdvertisementPickupAddressBuildingNumber,
-        string DefaultAdvertisementContactInfoEmail,
-        string DefaultAdvertisementContactInfoPhoneNumber);
-    Task<ICollection<PersonDto>> GetPersons();
-    Task<PersonDto> GetPerson(Guid id);
+        string Password);
     public Task<Guid> RegisterAsync(RegisterDto request);
-    public Task UpdatePerson(Guid id, UpdatePersonDto request);
-    public Task DeletePerson(Guid id);
+    public Task DeletePersonAsync(Guid id, string authToken);
 }
 
-public class AuthApiHttpClient(
-    HttpClient client,
-    ICurrentUserService currentUserService) : IAuthApiHttpClient
+public class AuthApiHttpClient(HttpClient client) : IAuthApiHttpClient
 {
     private static class Exceptions
     {
         public sealed class DeserializationOfApiResponseException() 
             : Exception("Something went wrong with deserialization of external api response");
-    }
-
-    private async Task SetUpAuthorization()
-    {
-        // string userId = currentUserService.UserId;
-        // ApplicationUser? user = await db.ApplicationUsers
-        //     .AsNoTracking()
-        //     .FirstOrDefaultAsync(x => x.Id.ToString() == userId);
-        // if (user is not null)
-        // {
-        //     var (token, _) = await jwtTokenService.GenerateTokenAsync(user);
-        //     client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-        // }
-    }
-    
-    public async Task<ICollection<IAuthApiHttpClient.PersonDto>> GetPersons()
-    {
-        await SetUpAuthorization();
-        HttpResponseMessage response = await client.GetAsync("v1/persons");
-        ICollection<IAuthApiHttpClient.PersonDto>? dtos = await response.Content.ReadFromJsonAsync<ICollection<IAuthApiHttpClient.PersonDto>>();
-        dtos ??= new List<IAuthApiHttpClient.PersonDto>();
-        return dtos;
-    }
-    
-    public async Task<IAuthApiHttpClient.PersonDto> GetPerson(Guid id)
-    {
-        await SetUpAuthorization();
-        HttpResponseMessage response = await client.GetAsync($"v1/persons/{id}");
-        IAuthApiHttpClient.PersonDto dto = await response.Content.ReadFromJsonAsync<IAuthApiHttpClient.PersonDto>()
-            ?? throw new Exceptions.DeserializationOfApiResponseException();
-        return dto;
     }
 
     public async Task<Guid> RegisterAsync(IAuthApiHttpClient.RegisterDto request)
@@ -126,35 +69,10 @@ public class AuthApiHttpClient(
         return idResponse.Id;
     }
     
-    public async Task UpdatePerson(Guid id, IAuthApiHttpClient.UpdatePersonDto request)
+    public async Task DeletePersonAsync(Guid id, string authHeader)
     {
-        await SetUpAuthorization();
-        string serializedRequest = JsonSerializer.Serialize(request);
-        StringContent bodyContent = new StringContent(serializedRequest, Encoding.UTF8, "application/json");
-        HttpResponseMessage response = await client.PutAsync($"v1/persons/{id}", bodyContent);
-        if (!response.IsSuccessStatusCode)
-        {
-            if (response.StatusCode is HttpStatusCode.BadRequest)
-            {
-                ValidationProblemDetails? validationProblemDetails = await response.Content.ReadFromJsonAsync<ValidationProblemDetails>();
-                if (validationProblemDetails is not null)
-                {
-                    throw new ApiValidationException(validationProblemDetails); 
-                }
-            }
-            ProblemDetails? problemDetails = await response.Content.ReadFromJsonAsync<ProblemDetails>();
-            if (problemDetails is not null)
-            {
-                throw new ApiException(problemDetails);
-            }
-            throw new Exceptions.DeserializationOfApiResponseException();
-        }
-    }
-    
-    public async Task DeletePerson(Guid id)
-    {
-        await SetUpAuthorization();
-        HttpResponseMessage response = await client.DeleteAsync($"v1/persons/{id}");
+        client.DefaultRequestHeaders.Authorization = AuthenticationHeaderValue.Parse(authHeader);
+        HttpResponseMessage response = await client.DeleteAsync($"v1/application-users/{id}");
         if (!response.IsSuccessStatusCode)
         {
             if (response.StatusCode is HttpStatusCode.BadRequest)
