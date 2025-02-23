34c20cd15e0c97ae3fac31a28c8282fd16d9782e
koniecdev <koniecdev@gmail.com>
2025-02-23 21:36:34 +0100

    Behaviour SmartEnum updated

--- src/KittySaver.Domain/Common/Primitives/Enums/Behaviour.cs
+++ src/KittySaver.Domain/Common/Primitives/Enums/Behaviour.cs
@@ -7,6 +7,7 @@ public sealed class Behavior : SmartEnumBase<Behavior>
 {
     public static readonly Behavior Friendly = new(nameof(Friendly), 0, 10);
     public static readonly Behavior Unfriendly = new(nameof(Unfriendly), 1, 5);
+    public static readonly Behavior Aggressive = new(nameof(Unfriendly), 1, 1);
     public static int MaxScorePoints => 10;
     public int ScorePoints { get; }
     private Behavior(string name, int value, int scorePoints) : base(name, value)
