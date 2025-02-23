038183a047a6229eb81aabe861cb371a47d906b7
koniecdev <koniecdev@gmail.com>
2025-02-23 21:34:32 +0100

    HealthStatus updated

--- src/KittySaver.Domain/Common/Primitives/Enums/HealthStatus.cs
+++ src/KittySaver.Domain/Common/Primitives/Enums/HealthStatus.cs
@@ -6,10 +6,14 @@ namespace KittySaver.Domain.Common.Primitives.Enums;
 public sealed class HealthStatus : SmartEnumBase<HealthStatus>
 {
     public static readonly HealthStatus Good = new(nameof(Good), 0, 10);
-    public static readonly HealthStatus Poor = new(nameof(Poor), 1, 5);
-    public static readonly HealthStatus Critical = new(nameof(Critical), 2, 1);
+    public static readonly HealthStatus Unknown = new(nameof(Unknown), 1, 7);
+    public static readonly HealthStatus ChronicMinor = new(nameof(ChronicMinor), 1, 5);
+    public static readonly HealthStatus ChronicSerious = new(nameof(ChronicSerious), 2, 2);
+    public static readonly HealthStatus Terminal = new(nameof(Terminal), 3, 1);
+    
     public static int MaxScorePoints => 10;
     public int ScorePoints { get; }
+
     private HealthStatus(string name, int value, int scorePoints) : base(name, value)
     {
         ScorePoints = scorePoints;
