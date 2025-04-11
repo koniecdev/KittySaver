30d7274ce350887097cf5901c0c47252cbdc211f
koniecdev <koniecdev@gmail.com>
2025-04-12 01:23:18 +0200

    New exception handlers added

---
+++ src/KittySaver.Api/Exceptions/Handlers/ApiExceptionHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+using KittySaver.Api.Infrastructure.Clients;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KittySaver.Api.Exceptions.Handlers;
+
+internal sealed class ApiExceptionHandler(ILogger<ApiExceptionHandler> logger) : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext,
+        Exception exception,
+        CancellationToken cancellationToken)
+    {
+        if (exception is not ApiException apiValidationEx)
+        {
+            return false;
+        }
+        
+        ProblemDetails problemDetails = apiValidationEx.ProblemDetails;
+        
+        logger.LogError(
+            exception,
+            "API Validation error occurred: {type} | {code} | {message}",
+            problemDetails.Status,
+            exception.GetType().Name,
+            exception.Message);
+
+        httpContext.Response.StatusCode = problemDetails.Status ?? StatusCodes.Status400BadRequest;
+        
+        await httpContext.Response
+            .WriteAsJsonAsync(problemDetails, cancellationToken);
+
+        return true;
+    }
+}
---
+++ src/KittySaver.Api/Exceptions/Handlers/ApiValidationExceptionHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+using KittySaver.Api.Infrastructure.Clients;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KittySaver.Api.Exceptions.Handlers;
+
+internal sealed class ApiValidationExceptionHandler(ILogger<ApiValidationExceptionHandler> logger) : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext,
+        Exception exception,
+        CancellationToken cancellationToken)
+    {
+        if (exception is not ApiValidationException apiValidationEx)
+        {
+            return false;
+        }
+        
+        ValidationProblemDetails problemDetails = apiValidationEx.ValidationProblemDetails;
+        
+        logger.LogError(
+            exception,
+            "API Validation error occurred: {type} | {code} | {message}",
+            problemDetails.Status,
+            exception.GetType().Name,
+            JsonSerializer.Serialize(problemDetails.Errors));
+
+        httpContext.Response.StatusCode = problemDetails.Status ?? StatusCodes.Status400BadRequest;
+        
+        await httpContext.Response
+            .WriteAsJsonAsync(problemDetails, cancellationToken);
+
+        return true;
+    }
+}
