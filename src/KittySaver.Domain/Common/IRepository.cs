a8a95fdc209de128f2ef8da38e3923cc83d1e738
koniecdev <koniecdev@gmail.com>
2025-04-15 14:02:31 +0200

    Generic repository pattern implemented

---
+++ src/KittySaver.Domain/Common/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace KittySaver.Domain.Common;
+
+public interface IRepository<TAggregateRoot, in TAggregateRootId> 
+    where TAggregateRootId : struct
+    where TAggregateRoot : AggregateRoot<TAggregateRootId>
+{
+    Task<TAggregateRoot> GetByIdAsync(TAggregateRootId id, CancellationToken cancellationToken);
+    void Insert(TAggregateRoot entity);
+    void Remove(TAggregateRoot entity);
+}
